36b906bb6d86878535a7025725dd844e459f9498
rr0055256 <raph_rahul@yahoo.co.in>
2020-07-21 10:03:36 +0530

    Added loop to display entered user

--- assignment_1/routes.js
+++ assignment_1/routes.js
@@ -1,4 +1,5 @@
 const routes = require('./routes');
+const users = []
 
 const requestHandler = (req, res) => {
     const url = req.url;
@@ -17,16 +18,15 @@ const requestHandler = (req, res) => {
         res.setHeader(`Content-Type`, `text/html`)
         res.write(`<html>`)
         res.write(`<head><title>Title</title></head>`)
-        res.write(`<body><ul><li>User 1</li></ul></body>`)
-        res.write(`<body><ul><li>User 2</li></ul></body>`)
+        var userText = ""
+        users.forEach((user) => {userText += `<body><ul>`+ user + `</body></ul>` });
+        res.write(userText)
         res.write(`<html>`)
         return res.end();
     }
 
     if (url === '/create-user' && method === `POST`) {
         const body = []
-        const users = []
-
         req.on("data", (chunk) => {
             console.log(chunk)
             body.push(chunk);
@@ -35,6 +35,8 @@ const requestHandler = (req, res) => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             users.push(parsedBody.split("=")[1])
+
+            
             console.log(users);
             
         })
